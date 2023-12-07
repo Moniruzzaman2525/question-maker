17350e8540a0252b8780628a1df7055f74740aff
MD MONIRUZZAMAN <96796294+Moniruzzaman2525@users.noreply.github.com>
2023-12-07 19:33:14 +0600

    done project

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <div>
             <div className="navbar bg-base-100">
                 <div className="flex-1">
-                    <a className="btn btn-ghost text-xl">Home</a>
+                    <NavLink to='/' className="btn btn-ghost text-xl">Home</NavLink>
                 </div>
                 <div className="flex-none">
                     <ul className="menu menu-horizontal flex gap-3 px-1">
